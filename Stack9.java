227c1e1ea3c95d860d02ba7f93b0f060969721fa
Jiya Chowdhury <138868637+jc-2402@users.noreply.github.com>
2024-08-17 23:21:40 +0530

    Add files via upload

--- Stack9.java
+++ Stack9.java
@@ -1,1 +1,41 @@
-//
+//valid parenthesis
+//given a string containg just the characters (,), {,}, [,] dtermine if the i/p string is valid
+import java.util.*;
+public class Stack9{
+    public static boolean isvalid(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            //opening
+            if(ch == '(' || ch == '{' || ch == '['){
+                s.push(ch);
+            }
+            else{
+                //closing
+                if(s.isEmpty()){
+                   return false; // eg - ")))"
+                }
+                //check pair
+                if( (s.peek() == '(' && ch == ')')
+                   || (s.peek() == '{' && ch == '}')
+                   || (s.peek() == '[' && ch == ']')){
+                    s.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(s.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        String str = "({}[])";
+        System.out.println(isvalid(str));
+    }
+}
